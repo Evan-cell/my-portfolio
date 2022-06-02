e97e5378e3cf041835b005cfdfe6e5cbef37f113
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-06-02 14:19:10 +0300

    useState

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Header.css"
 function Header() {
+    window.addEventListener("scroll", function () {
+        const header = document.querySelector(".header")
+        header.classList.toggle("active", window.scrollY > 100)
+      })
+      const [Mobile, setMobile] = useState(false)
   return (
     <header className='header'>
         <div className="container d-flex">
             <div className="logo">
-                <img src="" alt="" />
+                <img src="https://avatars.githubusercontent.com/u/60639510?v=4" alt="" />
             </div>
             <div className="nav-link">
                 <ul className="link f-flex uppercase">
@@ -19,7 +24,8 @@ function Header() {
                     <button className="home-btn">Hire me</button>
                 </ul>
                 <button className="toggle">
-                    <i className="fas fa-times"></i>
+                    <i className="fas fa-times home-btn close"></i>
+                    <i className="fas fa-bars open"></i>
                 </button>
             </div>
         </div>
