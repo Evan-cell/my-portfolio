5ec1bd442dc00f11d81eb6eda619f0bd3a2ec684
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-06-02 14:49:34 +0300

    Done with the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,60 @@
-import React, {useState} from 'react'
+import React, { useState } from "react"
 import "./Header.css"
-function Header() {
-    window.addEventListener("scroll", function () {
-        const header = document.querySelector(".header")
-        header.classList.toggle("active", window.scrollY > 100)
-      })
-      const [Mobile, setMobile] = useState(false)
+
+
+const Header = () => {
+  // fixed Header
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header")
+    header.classList.toggle("active", window.scrollY > 100)
+  })
+  // Toogle Menu
+  const [Mobile, setMobile] = useState(false)
   return (
-    <header className='header'>
-        <div className="container d-flex">
-            <div className="logo">
-                <img src="https://avatars.githubusercontent.com/u/60639510?v=4" alt="" />
-            </div>
-            <div className="nav-link">
-                <ul className="link f-flex uppercase">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#features">features</a></li>
-                    <li><a href="#portfolio">portfolio</a></li>
-                    <li><a href="#resume">resume</a></li>
-                    <li><a href="#clients">clients</a></li>
-                    <li><a href="#blogs">blogs</a></li>
-                    <li><a href="#contact">contact</a></li>
-                    <button className="home-btn">Hire me</button>
-                </ul>
-                <button className="toggle">
-                    <i className="fas fa-times home-btn close"></i>
-                    <i className="fas fa-bars open"></i>
-                </button>
-            </div>
+    <>
+      <header className='header'>
+        <div className='container d_flex'>
+          <div className='logo'>
+            <img src="https://avatars.githubusercontent.com/u/60639510?v=4" alt='' />
+          </div>
+
+          <div className='navlink'>
+            <ul className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"} onClick={() => setMobile(false)}>
+              {/*<ul className='link f_flex uppercase {Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}'>*/}
+              <li>
+                <a href='#home'>home</a>
+              </li>
+              <li>
+                <a href='#features'>features</a>
+              </li>
+              <li>
+                <a href='#portfolio'>portfolio</a>
+              </li>
+              <li>
+                <a href='#resume'>resume</a>
+              </li>
+              <li>
+                <a href='#clients'>clients</a>
+              </li>
+              <li>
+                <a href='#blog'>blog</a>
+              </li>
+              <li>
+                <a href='#contact'>contact</a>
+              </li>
+              <li>
+                <button className='home-btn'>Hire Me</button>
+              </li>
+            </ul>
+
+            <button className='toggle' onClick={() => setMobile(!Mobile)}>
+              {Mobile ? <i className='fas fa-times close home-btn'></i> : <i className='fas fa-bars open'></i>}
+            </button>
+          </div>
         </div>
-    </header>
+      </header>
+      <section className="demo"></section>
+    </>
   )
 }
 
